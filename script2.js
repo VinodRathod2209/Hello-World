0b3601097362b19f6a684799628e76b56f70ed52
VinodRathod2209 <44551454+VinodRathod2209@users.noreply.github.com>
2018-12-05 18:39:08 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,2 +1,4 @@
 console.log("this is a javascript attribute");
 alert("This is a alert message!!!!!!!");
+<Applet code="AppletTest" width="500" height="300"></Applet>
+<Applet code="AppletTest" width="500" height="450"></Applet>
